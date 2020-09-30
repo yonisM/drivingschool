993fa2d28290914f71fe646e235ef2a8b89e08a2
yonis mohamoud <yonis838@googlemail.com>
2020-09-30 22:13:02 +0100

    Change index to dropdown. Remove various codes including postcode stuff

--- server.py
+++ server.py
@@ -1,11 +1,8 @@
 from flask import Flask, render_template, request, send_file, send_from_directory, safe_join, abort
 import os
-from postcode import postcode_checker
 
 
 app = Flask(__name__)
-
-
 
 #Randomly generate secret keys
 SECRET_KEY = os.urandom(32)
@@ -17,16 +14,15 @@ app.config['SECRET_KEY'] = SECRET_KEY
 def index():
     show_hide = 'none'
     
+    
     if request.method == 'POST':
-        postcode = request.form['postcode']
-        evaluate_postcode = postcode_checker(postcode)
-        if evaluate_postcode == 'Yes':
-            return render_template('booking.html')
-        else:
+        area_selected = request.form['area']
+        if area_selected == 'Other':
             show_hide = 'block'
-            return render_template('index.html', evaluate_postcode = evaluate_postcode, show_hide = show_hide)
-
-        return render_template('index.html', evaluate_postcode = evaluate_postcode, show_hide = show_hide)
+            evaluate_area = "Sorry, we only serve Wood Green, Chingford, and Enfield areas"
+            return render_template('index.html', show_hide = show_hide, evaluate_area = evaluate_area)
+        else:
+            return render_template('booking.html')
     else:
         return render_template('index.html', show_hide = show_hide)
 
